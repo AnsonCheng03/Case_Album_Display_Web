f48c4311bf512c0eeca9338c70a5e4087aa95da3
Anson Cheng <Anson.12666@icloud.com>
2024-01-19 18:39:44 +0800

    chore: fix lint error

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -52,7 +52,7 @@ export default component$(() => {
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14764.451576549996!2d114.21059257396709!3d22.311569774854092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34040146068751a1%3A0xf453cc7260f035b4!2z6KeA5aGY6bS75ZyW6YGTNDLomZ_oj6_lr7bkuK3lv4M!5e0!3m2!1szh-TW!2shk!4v1685814816082!5m2!1szh-TW!2shk"
                 loading="lazy"
                 class={styles.mapFrame}
-                allowFullScreen
+                allowFullscreen
                 referrerPolicy="no-referrer-when-downgrade"
               />
             </div>
