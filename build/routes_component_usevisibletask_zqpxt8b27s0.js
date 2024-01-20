b5dadac4633dd399377bebbcd8807ffd30fa2c9c
AnsonCheng03 <AnsonCheng03@users.noreply.github.com>
2024-01-20 06:55:59 +0000

    deploy: d4559b938020a015bf7de8fe57d5e28080d64330

--- build/routes_component_usevisibletask_zqpxt8b27s0.js
+++ build/routes_component_usevisibletask_zqpxt8b27s0.js
@@ -1,1 +1,1 @@
-import{v as i}from"./root2.js";import{A as l}from"./root2.js";const p=async()=>{const[t,a,e]=i(),s=await t();try{const r=(await s.json()).map(o=>({...o,path:e.url.origin+"/"+o.path}));a.value=r}catch{a.value=[[]]}};export{l as _hW,p as routes_component_useVisibleTask_zqpXT8B27s0};
+import{v as n}from"./root2.js";import{A as h}from"./root2.js";const u=async()=>{const[t,e,a,s]=n(),c=await e();try{const i=(await c.json()).map(o=>({...o,path:s.url.origin+`${t}/`+o.path}));a.value=i}catch{a.value=[[]]}};export{h as _hW,u as routes_component_useVisibleTask_zqpXT8B27s0};
