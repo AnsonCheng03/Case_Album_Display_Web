eff016099db129ad603f0f157bb874b703e36638
AnsonCheng03 <AnsonCheng03@users.noreply.github.com>
2024-01-20 07:19:30 +0000

    deploy: 313d2270bea27086d8c4755aca38502618f6d175

--- build/routes_component_fetchimagesource_hokcw92ockc.js
+++ build/routes_component_fetchimagesource_hokcw92ockc.js
@@ -1,1 +1,1 @@
-import{v as t}from"./root2.js";import{A as a}from"./root2.js";const r=()=>{const[o,e]=t();return fetch(e.url.origin+`${o}/Images.json?t=`+Date.now(),{cache:"no-store"})};export{a as _hW,r as routes_component_fetchImageSource_HOkCW92ocKc};
+import{v as t}from"./root2.js";import{A as a}from"./root2.js";const r=()=>{const[o,e]=t();return console.log("fetchImageSource",e.url.origin+`${o}/Images.json?t=`+Date.now()),fetch(e.url.origin+`${o}/Images.json?t=`+Date.now(),{cache:"no-store"})};export{a as _hW,r as routes_component_fetchImageSource_HOkCW92ocKc};
