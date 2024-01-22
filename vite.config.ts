94339005373aa5b915a99d7533bd2ffce5161b2d
Anson Cheng <Anson.12666@icloud.com>
2024-01-22 13:02:29 +0800

    chore: add / to base

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,9 @@ dotenv.config()
 
 
 export default defineConfig(() => {
-  console.log('process.env.PUBLIC_BASE_URL', process.env.PUBLIC_BASE_URL)
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-    base: process.env.PUBLIC_BASE_URL,
+    base: `${process.env.PUBLIC_BASE_URL}/`,
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
